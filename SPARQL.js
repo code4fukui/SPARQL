33016f1c2376d469a524ea1aa6680b1f9cfe6836
Taisuke Fukuno <fukuno@jig.jp>
2022-05-05 06:38:32 +0900

    Initial commit

---
+++ SPARQL.js
@@ -0,0 +1,12 @@
+class SPARQL {
+  constructor(endpoint) {
+    this.endpoint = endpoint;
+  }
+  query(q) {
+    const endpoint = "https://sparql.odp.jig.jp/api/v1/sparql";
+    const url = endpoint + "?output=json&query=" + encodeURIComponent(q);
+    return await (await fetch(url)).json();
+  }
+}
+
+export { SPARQL };
